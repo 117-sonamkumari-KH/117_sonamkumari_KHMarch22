e51a9c0c9a7c8d0f32ba6a3b68a2b10a8e03bf7e
117-sonamkumari-KH <101339069+117-sonamkumari-KH@users.noreply.github.com>
2022-05-04 14:55:29 +0530

    Add files via upload

---
+++ assignment 3/Nquestion1_10.java
@@ -0,0 +1,20 @@
+	 
+	 class Nquestion1_11
+	 {
+	 public static void main(String args[])
+	 {
+		 
+	 for(int i=5;i>=1;i--)
+	 {
+		 for(int j = 1;j<=i;j++)
+		 {
+			 System.out.print(" ");
+		 }
+		 for(int j=5-i;j>=0;j--)
+		 {
+			 System.out.print((char)(letter-j)+" ");
+		 }
+		 System.out.println();
+	 }
+	 }
+     }
